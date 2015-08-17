8bd1137799a8e0ddf9ff65a92d10a9bdbae51d7b
Michal Kijowski <michal.kijowski@hotmail.com>
2015-08-17 13:07:58 +0100

    Add column sorting capabilities

--- ts/TabularFileComponent.tsx
+++ ts/TabularFileComponent.tsx
@@ -1,18 +1,15 @@
 import React = require("react");
 
 interface RowProps {
-  row : string
-  delimeter : string
+  row : string[]
   key : string
 }
 
 class RowComponent extends React.Component<RowProps,void>{
 
   render() {
-    let columns = this.props.row.split(this.props.delimeter);
-
     return <tr>
-      {columns.map((column, index) => <td key={index}>{column}</td>)}
+      {this.props.row.map((column, index) => <td key={index}>{column}</td>)}
       </tr>
   }
 }
@@ -21,11 +18,63 @@ interface FileProps {
   content : string
   delimeter : string
 }
+interface FileState {
+  orderByColumn : number
+  ascendingOrder : boolean
+}
 
-class FileComponent extends React.Component<FileProps,void> {
+class FileComponent extends React.Component<FileProps,FileState> {
+    state : FileState = {
+      orderByColumn : -1,
+      ascendingOrder : true
+    }
+    getDifferences = (other:FileComponent) => {
+
+    }
+    maxLength : number;
+
+    processRows = (rows:string[]) => {
+      let rowComparer = (x,y) => {
+        let firstItem = x[this.state.orderByColumn]
+        let secondItem = y[this.state.orderByColumn]
+        let direction = this.state.ascendingOrder?1:-1;
+        if(typeof firstItem === 'undefined' && typeof secondItem === 'undefined'){
+          return 0;
+        }
+        if(typeof firstItem === 'undefined'){
+          return -1 * direction;
+        }
+        if(typeof secondItem === 'undefined'){
+          return 1 * direction;
+        }
+        else{
+          return firstItem.localeCompare(secondItem) * direction;
+        }
+      }
+
+      let splittedRows = rows.map(row => row.split(this.props.delimeter))
+      this.maxLength = Math.max.apply(Math,splittedRows.map((x) => x.length))
+      if (this.state.orderByColumn >= 0) {
+        return splittedRows.sort((x,y) => rowComparer(x,y))
+      }
+      return splittedRows
+    }
+
+    onHeaderClick = (mouseEvent) => {
+      let newDirection = this.state.orderByColumn == mouseEvent?!this.state.ascendingOrder:true;
+      this.setState({orderByColumn : mouseEvent, ascendingOrder : newDirection})
+    }
+
     render(){
-      let rows = this.props.content.split("\n");
-      return <table><tbody>{rows.map((row, index) => <RowComponent key={index.toString()} row={row} delimeter = {this.props.delimeter}/>)}</tbody></table>
+      let rows = this.processRows(this.props.content.split("\n"));
+      let headers =[];
+      for (let i = 0; i < this.maxLength; i++) {
+        headers[i] = i;
+      }
+      return <table>
+              <thead><tr>{headers.map((row) => <td key={row} onClick={this.onHeaderClick.bind(null,row)}>Column {row}</td>)}</tr></thead>
+              <tbody>{rows.map((row, index) => <RowComponent key={index.toString()} row={row}/>)}</tbody>
+             </table>
     }
 }
 
