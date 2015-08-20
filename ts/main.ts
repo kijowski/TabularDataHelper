45e040c9b39a134a38226e5a8ec083d4d1d36f30
Michal Kijowski <michal.kijowski@hotmail.com>
2015-08-20 18:19:35 +0100

    Remove development files

--- ts/main.ts
+++
@@ -1,7 +0,0 @@
-import React = require('react');
-import tfc = require('./components');
-
-window.onload = function () {
-  var element = React.createElement(tfc.TabularDataWrapper);
-  React.render(element, document.getElementById("mount"));
-}
--- typings/tsd.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="react/react.d.ts" />
